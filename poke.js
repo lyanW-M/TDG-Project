e0ca7820460cc7226af16054bcdfdab278d4e3a0
lyanW-M <lyanw@wsc.school.nz>
2025-05-04 22:50:35 +1200

    #10

---
+++ poke.js
@@ -0,0 +1,92 @@
+const pokedex = [
+    {
+      number: "001",
+      name: "Bulbasaur",
+      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+      types: ["Grass", "Poison"],
+      height: "0.7 m (2'04\")",
+      weight: "6.9 kg (15.2 lbs)",
+      abilities: "Overgrow, Chlorophyll (hidden ability)",
+      description: "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger."
+    },
+    {
+      number: "002",
+      name: "Ivysaur",
+      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
+      types: ["Grass", "Poison"],
+      height: "1.0 m (3'03\")",
+      weight: "13.0 kg (28.7 lbs)",
+      abilities: "Overgrow, Chlorophyll (hidden ability)",
+      description: "There is a bud on this Pokémon's back. To support its weight, Ivysaur's legs and trunk grow thick and strong. If it starts spending more time lying in the sunlight, it's a sign that the bud will bloom into a large flower soon."
+    },
+    {
+      number: "003",
+      name: "Venusaur",
+      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
+      types: ["Grass", "Poison"],
+      height: "2.0 m (6'07\")",
+      weight: "100.0 kg (220.5 lbs)",
+      abilities: "Overgrow, Chlorophyll (hidden ability)",
+      description: "There is a large flower on Venusaur's back. The flower is said to take on vivid colors if it gets plenty of nutrition and sunlight. The flower's aroma soothes the emotions of people."
+    },
+    {
+      number: "004",
+      name: "Charmander",
+      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+      types: ["Fire"],
+      height: "0.6 m (2'00\")",
+      weight: "8.5 kg (18.7 lbs)",
+      abilities: "Blaze, Solar Power (hidden ability)",
+      description: "The flame that burns at the tip of its tail is an indication of its emotions. The flame wavers when Charmander is enjoying itself. If the Pokémon becomes enraged, the flame burns fiercely."
+    },
+    {
+      number: "005",
+      name: "Charmeleon",
+      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png",
+      types: ["Fire"],
+      height: "1.1 m (3'07\")",
+      weight: "19.0 kg (41.9 lbs)",
+      abilities: "Blaze, Solar Power (hidden ability)",
+      description: "Charmeleon mercilessly destroys its foes using its sharp claws. If it encounters a strong foe, it turns aggressive. In this excited state, the flame at the tip of its tail flares with a bluish white color."
+    },
+    {
+      number: "006",
+      name: "Charizard",
+      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
+      types: ["Fire", "Flying"],
+      height: "1.7 m (5'07\")",
+      weight: "90.5 kg (199.5 lbs)",
+      abilities: "Blaze, Solar Power (hidden ability)",
+      description: "Charizard flies around the sky in search of powerful opponents. It breathes fire of such great heat that it melts anything. However, it never turns its fiery breath on any opponent weaker than itself."
+    },
+    {
+      number: "007",
+      name: "Squirtle",
+      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+      types: ["Water"],
+      height: "0.5 m (1'08\")",
+      weight: "9.0 kg (19.8 lbs)",
+      abilities: "Torrent, Rain Dish (hidden ability)",
+      description: "Squirtle's shell is not merely used for protection. The shell's rounded shape and the grooves on its surface help minimize resistance in water, enabling this Pokémon to swim at high speeds."
+    },
+    {
+      number: "008",
+      name: "Wartortle",
+      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png",
+      types: ["Water"],
+      height: "1.0 m (3'03\")",
+      weight: "22.5 kg (49.6 lbs)",
+      abilities: "Torrent, Rain Dish (hidden ability)",
+      description: "Its tail is large and covered with a rich, thick fur. The tail becomes increasingly deeper in color as Wartortle ages. The scratches on its shell are evidence of this Pokémon's toughness as a battler."
+    },
+    {
+      number: "009",
+      name: "Blastoise",
+      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png",
+      types: ["Water"],
+      height: "1.6 m (5'03\")",
+      weight: "85.5 kg (188.5 lbs)",
+      abilities: "Torrent, Rain Dish (hidden ability)",
+      description: "Blastoise has water spouts that protrude from its shell. The water spouts are very accurate. They can shoot bullets of water with enough accuracy to strike empty cans from a distance of over 160 feet."
+    }
+  ];
